ce86278f8eca7b0b78a1674386cf36a5c20fbf8f
kenhaesler <50946664+kenhaesler@users.noreply.github.com>
2021-01-06 16:19:32 +0100

    Changed the files to C

--- NimGame/NimGame.c
+++ NimGame/NimGame.c
@@ -1,12 +1,16 @@
-// NimGame.cpp
-
-//Header files
-#include <iostream>
 #include <stdio.h>      // rpintf, scanf
 #include <stdint.h>     // int32_t
 #include <stdlib.h>     // rand()
 #include <time.h>       // initialisierung des Zufallsgenerators
 
+/*int32_t ReadNumber()
+{
+    int32_t number;
+    scanf_s("%i", &number);
+    while (getchar() != '\n'); // this is needed to clear the buffer!
+    return number;
+}
+*/
 
 int main()
 {
