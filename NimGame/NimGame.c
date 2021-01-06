5d8ab6348c7f8a22bb682f417837a0f12ed49d12
kenhaesler <50946664+kenhaesler@users.noreply.github.com>
2021-01-06 16:11:17 +0100

    Initial commit

---
+++ NimGame/NimGame.c
@@ -0,0 +1,14 @@
+// NimGame.cpp
+
+//Header files
+#include <iostream>
+#include <stdio.h>      // rpintf, scanf
+#include <stdint.h>     // int32_t
+#include <stdlib.h>     // rand()
+#include <time.h>       // initialisierung des Zufallsgenerators
+
+
+int main()
+{
+// Here starts everything...
+}
