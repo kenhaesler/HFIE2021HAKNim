416e67ba9ae0938f4f2a5f0b36e2a8cf6cf40146
Ken Häsler <kenhaesler@outlook.com>
2021-01-12 19:23:10 +0100

    Nim game now accepts userinput. Taking matches is possible

--- NimGame/NimGame.c
+++ NimGame/NimGame.c
@@ -3,16 +3,74 @@
 #include <stdlib.h>     // rand()
 #include <time.h>       // initialisierung des Zufallsgenerators
 
-/*int32_t ReadNumber()
+
+#define foreach(item, array) \
+    for(int keep = 1, \
+            count = 0,\
+            size = sizeof (array) / sizeof *(array); \
+        keep && count != size; \
+        keep = !keep, count++) \
+      for(item = (array) + count; keep; keep = !keep)
+
+
+int32_t ReadNumber()
 {
     int32_t number;
     scanf_s("%i", &number);
     while (getchar() != '\n'); // this is needed to clear the buffer!
     return number;
 }
-*/
+
+
+void showNimGame(int nimdata[]);
+
+void AskUser(int* arraylocation, int* num);
+
 
 int main()
 {
-// Here starts everything...
+    int startflag = 0;
+    char startgame[10];
+    int num;
+    int arraylocation;
+
+    //while (startflag != 1) {
+        int nimdata[4] = { 1,3,5,7 };
+        showNimGame(nimdata);
+        AskUser(&arraylocation, &num);
+        //printf("Arraylocation: %i", arraylocation+1);
+        if (nimdata[arraylocation] - num >= 0) {
+            nimdata[arraylocation] = nimdata[arraylocation] - num;
+        }
+        else { printf("Die Zahl ist zu hoch!"); }
+        
+        showNimGame(nimdata);
+    //}
+}
+
+
+void showNimGame(int nimdata[]) {
+    int i;
+    int count;
+    for (i = 0; 3 >= i; i++) {
+        //printf("Die Zahl im array ist %i\n", nimdata[i]);
+        if (i < 1) { printf("\n"); }
+        for (count = nimdata[i] + 1; count > 1; count--) {
+            printf("I ");
+        }
+        printf("\n");
+    }
+}
+
+
+void AskUser(int* arraylocation, int* num) {
+    printf("\nWhich row: ");
+    scanf_s("%i", arraylocation);
+
+   *arraylocation = *arraylocation - 1;
+    //printf("Arraylocation in function: %i", *arraylocation);
+
+
+    printf("\nHow much: ");
+    scanf_s("%i", num);
 }
